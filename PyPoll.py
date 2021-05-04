e67bf82ba43590521720eb6b94cbfa3db1aeb11c
nylaivy <41087395+nylaivy@users.noreply.github.com>
2021-05-03 20:05:43 -0400

    Adding PyPoll file to repository

---
+++ PyPoll.py
@@ -0,0 +1,36 @@
+import csv
+import os
+
+
+#The data we need to retrieve
+#1. The total number of votes cast
+#2. A complete list of candidates who received votes
+#3. The percentage of votes each candidate won
+#4. The total number of votes each candidate won
+#5. The winner of the election based on popular vote
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print each row in the CSV file.
+    #for row in file_reader:
+     #   print(row)
+    
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+# Using the with statement open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
+
+     # Write three counties to the file.
+     #txt_file.write("Arapahoe\nDenver\nJefferson")
